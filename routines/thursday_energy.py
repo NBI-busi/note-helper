f55ba99474cf5e07fa92167e47611f97463fd89e
NBI-busi <ibonne181100@gmail.com>
2025-07-22 15:26:56 +0900

    Add files via upload

---
+++ routines/thursday_energy.py
@@ -0,0 +1,13 @@
+from config import thursday as cfg
+from common.ui_common import render_ui
+from common.draw_cftc_graph import draw_cftc_graph
+
+def run():
+    render_ui(
+        title=cfg.title,
+        steps=cfg.steps,
+        buttons=cfg.buttons,
+        futures_files=cfg.futures_files,
+        options_files=cfg.options_files,
+        draw_func=draw_cftc_graph
+    )
