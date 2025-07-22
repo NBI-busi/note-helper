c84d667ebac57baaf24372719bb33ad674e32264
NBI-busi <ibonne181100@gmail.com>
2025-07-22 15:27:42 +0900

    Add files via upload

---
+++ config/thursday.py
@@ -0,0 +1,14 @@
+title = "🔌 木曜ルーティン：エネルギー商品 ファンダメンタルズ整理"
+
+steps = [
+    "IEA[国際エネルギー機関]の月次レポートの確認(https://www.iea.org/), 月1回・中旬頃",
+    "IEA[米国エネルギー情報局]の週次レポートの確認(https://www.eia.gov/), 毎週",
+    "CFTC発表の機関投資家のポジション確認, 毎週発表(https://www.cftc.gov/...)"
+]
+
+buttons = [
+    "", "", ""
+]
+
+futures_files = ["f_year_2025.csv", "f_year_2024.csv", "f_year_2023.csv"]
+options_files = ["c_year_2025.csv", "c_year_2024.csv", "c_year_2023.csv"]
---
+++ config/tuesday.py
@@ -0,0 +1,18 @@
+title = "🌽 火曜ルーティン：エネルギー商品 ファンダメンタルズ整理"
+
+steps = [
+        "WASDEの月次レポートの確認(https://www.usda.gov/about-usda/general-information/staff-offices/office-chief-economist/commodity-markets/wasde-report), 和訳(https://www.maff.go.jp/j/zyukyu/jki/j_usda/), 月1回・10日頃",
+        "USDA[米国農務省情報局]の週次レポートの確認(https://usda.library.cornell.edu/concern/publications/8336h188j), 毎週",
+        "Brownfield Ag. Newsの日次ニュースの確認(https://www.youtube.com/@BrownfieldAgNews)",
+        "発表の機関投資家のポジション確認, 毎週発表(https://www.cftc.gov/MarketReports/CommitmentsofTraders/HistoricalCompressed/index.htm)"
+    ]
+
+buttons = [
+        "",         # 手順1のボタン名
+        "",         # 手順2のボタン名
+        "",         # 手順3のボタン名
+        ""          # 手順4は特別処理（複数ボタン対応）
+    ]
+
+futures_files = ["f_year_2025.csv", "f_year_2024.csv", "f_year_2023.csv"]
+options_files = ["c_year_2025.csv", "c_year_2024.csv", "c_year_2023.csv"]
