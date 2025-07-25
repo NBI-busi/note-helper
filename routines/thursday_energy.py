f376d7caea71ade5f4cdde5ec80e1bf0c1227212
NBI-busi <ibonne181100@gmail.com>
2025-07-25 12:26:48 +0900

    Add files via upload

--- routines/thursday_energy.py
+++ routines/thursday_energy.py
@@ -2,6 +2,7 @@ from config import thursday as cfg
 from common.ui_common import render_ui
 from common.draw_cftc_graph import draw_cftc_graph
 
+#show daily procedures at upper
 def run():
     render_ui(
         title=cfg.title,
